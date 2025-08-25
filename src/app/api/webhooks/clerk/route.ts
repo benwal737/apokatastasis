9532129ae3a4d4503cc5437555f07affc14b2ce7
Ben Walderman <benlwalderman@gmail.com>
2025-08-24 19:08:24 -0700

    clerk webhook steup

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,20 @@
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+
+    const { id } = evt.data;
+    if (!id) throw new Error("No user ID found in webhook payload");
+    const eventType = evt.type;
+    if (eventType === "user.created") {
+      console.log("User created:", id);
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
