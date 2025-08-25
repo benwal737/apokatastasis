bacda6b6816db5115e2cf83a873f549175da1eb4
Ben Walderman <benlwalderman@gmail.com>
2025-08-24 19:37:53 -0700

    user.created webhook

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,15 +1,22 @@
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
-
+import prisma from "@/lib/prisma";
 export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
 
-    const { id } = evt.data;
-    if (!id) throw new Error("No user ID found in webhook payload");
     const eventType = evt.type;
     if (eventType === "user.created") {
-      console.log("User created:", id);
+      console.log("User created:", evt.data.id);
+      await prisma.user.create({
+        data: {
+          username:
+            evt.data.username ||
+            evt.data.email_addresses[0].email_address.split("@")[0],
+          imageUrl: evt.data.image_url!,
+          externalUserId: evt.data.id!,
+        },
+      });
     }
 
     return new Response("Webhook received", { status: 200 });
